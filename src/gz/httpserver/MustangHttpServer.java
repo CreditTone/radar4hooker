2cdfe3591c43917d000331466fee56f85121c1bb
guozhong <1273568669@qq.com>
2021-01-10 10:36:24 -0800

    add mustang http server

---
+++ src/gz/httpserver/MustangHttpServer.java
@@ -0,0 +1,37 @@
+package gz.httpserver;
+
+import java.util.ArrayList;
+import java.util.List;
+import gz.httpserver.EmbeddHTTPServer;
+
+public class MustangHttpServer extends EmbeddHTTPServer {
+
+    protected final List<MustangController> mustangControllers = new ArrayList<>();
+
+    public MustangHttpServer(int port) {
+        super(port);
+    }
+
+    public void addController(MustangController mustangController) {
+        mustangControllers.add(mustangController);
+    }
+
+    protected String checkPath(String originPath) {
+        String urlPath = originPath.replaceAll("[/]{2,}", "/");
+        if (urlPath.contains("?")) {
+            urlPath = urlPath.split("\\?")[0];
+        }
+        return urlPath;
+    }
+
+    @Override
+    public String onResponse(String path, EmbeddHTTPParams embeddHTTPParams) throws Exception {
+        String urlPath = checkPath(path);
+        for (MustangController mustangController : mustangControllers) {
+            if (mustangController.path.equals(urlPath)) {
+                return mustangController.callOnResponse(embeddHTTPParams);
+            }
+        }
+        return "{msg:\"Not found the path '"+path+"'.\"}";
+    }
+}
