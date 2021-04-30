cdf43cea3b5056fe03dfcbcf6f4876da63c08529
郭钟 <guozhong@quicklyun.com>
2021-04-30 15:28:15 +0800

    fix

--- src/gz/justtrustme/OkHttp3FakeFinder.java
+++ src/gz/justtrustme/OkHttp3FakeFinder.java
@@ -3,43 +3,51 @@ package gz.justtrustme;
 import java.lang.reflect.Field;
 import java.lang.reflect.Method;
 import java.lang.reflect.Modifier;
-import java.lang.reflect.Parameter;
+
+import gz.justtrustme.Okhttp3FakeClass.FakeMethod;
+import gz.util.XLog;
 
 public class OkHttp3FakeFinder {
-	
+
 	// public static final class Builder
-	public static String[] okHttpClientBuilderCheck(String className) {
-		if (className.startsWith("java.") || className.startsWith("android") || className.startsWith("com")
-				|| className.startsWith("[") || !className.contains("$")) {
-			return null;
-		}
-		//java.net.Proxy proxy;
+	public static Okhttp3FakeClass okHttpClientBuilderCheck(Class clz) {
+		// java.net.Proxy proxy;
 		boolean hasProxyField = false;
-		
-		//java.net.ProxySelector proxySelector;
+
+		// java.net.ProxySelector proxySelector;
 		boolean hasProxySelectorField = false;
-		
-		//javax.net.SocketFactory socketFactory;
+
+		// javax.net.SocketFactory socketFactory;
 		boolean hasSocketFactoryField = false;
-		
-        //javax.net.ssl.SSLSocketFactory sslSocketFactory;
+
+		// javax.net.ssl.SSLSocketFactory sslSocketFactory;
 		boolean hasSSLSocketFactoryField = false;
-		
-		//javax.net.ssl.HostnameVerifier
+
+		// javax.net.ssl.HostnameVerifier
 		boolean hasHostnameVerifierField = false;
-		
-		//public Builder sslSocketFactory(javax.net.ssl.SSLSocketFactory sSLSocketFactory)
+
+		// public Builder sslSocketFactory(javax.net.ssl.SSLSocketFactory
+		// sSLSocketFactory)
 		boolean hasSslSocketFactoryMethod = false;
 		String sslSocketFactoryMethodName = null;
-		
-		//public Builder sslSocketFactory(javax.net.ssl.SSLSocketFactory sSLSocketFactory, javax.net.ssl.X509TrustManager x509TrustManager)
+
+		// public Builder sslSocketFactory(javax.net.ssl.SSLSocketFactory
+		// sSLSocketFactory, javax.net.ssl.X509TrustManager x509TrustManager)
 		boolean hasSslSocketFactory2Method = false;
 		String sslSocketFactoryMethod2Name = null;
-		
+
 		try {
-			Class<?> clz = Class.forName(className);
-			//校验 public static final class Builder 类声明
-			if (!Modifier.isFinal(clz.getModifiers()) || !Modifier.isPublic(clz.getModifiers()) || !Modifier.isStatic(clz.getModifiers()) ) {
+			String className = clz.getName();
+			if (clz.isInterface() || clz.isArray() || clz.isAnnotation() || clz.isEnum()
+					|| className.startsWith("java.") || className.startsWith("android") || className.startsWith("com")
+					|| !className.contains("$")) {
+				// XLog.appendText("okHttpClientBuilderCheck 111");
+				return null;
+			}
+			// 校验 public static final class Builder 类声明
+			if (!Modifier.isFinal(clz.getModifiers()) || !Modifier.isPublic(clz.getModifiers())
+					|| !Modifier.isStatic(clz.getModifiers())) {
+				// XLog.appendText("okHttpClientBuilderCheck 222");
 				return null;
 			}
 			Field[] fields = clz.getDeclaredFields();
@@ -47,74 +55,81 @@ public class OkHttp3FakeFinder {
 				String fieldClassName = field.getType().getName();
 				if ("java.net.Proxy".equals(fieldClassName)) {
 					hasProxyField = true;
-				}else if ("java.net.ProxySelector".equals(fieldClassName)) {
+				} else if ("java.net.ProxySelector".equals(fieldClassName)) {
 					hasProxySelectorField = true;
-				}else if ("javax.net.SocketFactory".equals(fieldClassName)) {
+				} else if ("javax.net.SocketFactory".equals(fieldClassName)) {
 					hasSocketFactoryField = true;
-				}else if ("javax.net.ssl.SSLSocketFactory".equals(fieldClassName)) {
-					hasSocketFactoryField = true;
-				}else if ("javax.net.ssl.HostnameVerifier".equals(fieldClassName)) {
+				} else if ("javax.net.ssl.SSLSocketFactory".equals(fieldClassName)) {
+					hasSSLSocketFactoryField = true;
+				} else if ("javax.net.ssl.HostnameVerifier".equals(fieldClassName)) {
 					hasHostnameVerifierField = true;
 				}
 			}
-			
 			Method[] methods = clz.getDeclaredMethods();
 			for (int i = 0; i < methods.length; i++) {
 				Method method = methods[i];
-				//如果方法非public或static方法直接continue
+				// 如果方法非public或static方法直接continue
 				if (!Modifier.isPublic(method.getModifiers()) || Modifier.isStatic(method.getModifiers())) {
 					continue;
 				}
-				//校验方法返回值为Builder自己
+				// 校验方法返回值为Builder自己
 				if (!clz.equals(method.getReturnType())) {
 					continue;
 				}
-				
-				if (method.getParameterCount() == 1) {
-					Parameter parameter = method.getParameters()[0];
-					if ("javax.net.ssl.SSLSocketFactory".equals(parameter.getType().getName())) {
+				Class<?>[] parameterTypes = method.getParameterTypes();
+				int parameterCount = parameterTypes.length;
+				if (parameterCount == 1) {
+					if ("javax.net.ssl.SSLSocketFactory".equals(parameterTypes[0].getName())) {
 						hasSslSocketFactoryMethod = true;
-						sslSocketFactoryMethodName = method.getName();
+						sslSocketFactoryMethodName = method.toString();
 					}
-				} else if (method.getParameterCount() == 2) {
-					Parameter parameter0 = method.getParameters()[0];
-					Parameter parameter1 = method.getParameters()[1];
-					if ("javax.net.ssl.SSLSocketFactory".equals(parameter0.getType().getName())
-							&& "javax.net.ssl.X509TrustManager".equals(parameter1.getType().getName())) {
+				} else if (parameterCount == 2) {
+					Class parameter0Clz = parameterTypes[0];
+					Class parameter1Clz = parameterTypes[1];
+					if ("javax.net.ssl.SSLSocketFactory".equals(parameter0Clz.getName())
+							&& "javax.net.ssl.X509TrustManager".equals(parameter1Clz.getName())) {
 						hasSslSocketFactory2Method = true;
-						sslSocketFactoryMethod2Name = method.getName();
+						sslSocketFactoryMethod2Name = method.toString();
 					}
 				}
 			}
-			
-			//7项核心指标全部达标，我们可以判断这个类就是okhttp3.OkHttpClient$Builder的混淆类了
-			if (hasProxyField && hasProxySelectorField && hasSocketFactoryField 
-					&& hasSSLSocketFactoryField && hasHostnameVerifierField && hasSslSocketFactoryMethod 
-					&& hasSslSocketFactory2Method) {
-				//返回混淆的类名和方法名
-				return new String[] {className, sslSocketFactoryMethodName, sslSocketFactoryMethod2Name};
-			}
-		} catch (Exception e) {
+		} catch (Throwable e) {
+			// XLog.appendText(new Exception(e));
 		}
-		
+		// 7项核心指标全部达标，我们可以判断这个类就是okhttp3.OkHttpClient$Builder的混淆类了
+		if (hasProxyField && hasProxySelectorField && hasSocketFactoryField && hasSSLSocketFactoryField
+				&& hasHostnameVerifierField) {
+			// 返回混淆的类名和方法名
+			Okhttp3FakeClass okhttp3FakeClass = new Okhttp3FakeClass("okhttp3.OkHttpClient$Builder", clz.getName());
+			if (hasSslSocketFactoryMethod && hasSslSocketFactory2Method) {
+				okhttp3FakeClass.addFakeMethod(new FakeMethod(
+						"public okhttp3.OkHttpClient$Builder okhttp3.OkHttpClient$Builder.sslSocketFactory(javax.net.ssl.SSLSocketFactory)",
+						sslSocketFactoryMethodName));
+				okhttp3FakeClass.addFakeMethod(new FakeMethod(
+						"public okhttp3.OkHttpClient$Builder okhttp3.OkHttpClient$Builder.sslSocketFactory(javax.net.ssl.SSLSocketFactory,javax.net.ssl.X509TrustManager)",
+						sslSocketFactoryMethod2Name));
+			}
+			return okhttp3FakeClass;
+		}
 		return null;
 	}
 
 	// public final class okhttp3.CertificatePinner
-	public static String[] okHttpCertificatePinnerCheck(String className) {
-		if (isInvalidOkHttpClass(className)) {
-			return null;
-		}
+	public static Okhttp3FakeClass okHttpCertificatePinnerCheck(Class clz) {
 		// public void check(String str, List<Certificate> list)
-		boolean sameCheckMethod = false;
+		boolean hasCheckMethod = false;
 		// public static String pin(Certificate certificate)
-		boolean samePinMethod = false;
+		boolean hasPinMethod = false;
 		// public static ByteString sha1(X509Certificate x509Certificate)
-		boolean sameSha1Method = false;
+		boolean hasSha1Method = false;
 		String checkMethodName = null;
 		try {
-			Class<?> clz = Class.forName(className);
-			if (clz.isInterface()) {
+			if (isInvalidOkHttpClass(clz)) {
+				return null;
+			}
+			Class superClz = clz.getSuperclass();
+			if (!superClz.equals(Object.class) || !Modifier.isPublic(clz.getModifiers())
+					|| !Modifier.isFinal(clz.getModifiers())) {
 				return null;
 			}
 			if (!Modifier.isFinal(clz.getModifiers())) {
@@ -123,98 +138,114 @@ public class OkHttp3FakeFinder {
 			Method[] methods = clz.getDeclaredMethods();
 			for (int i = 0; i < methods.length; i++) {
 				Method method = methods[i];
-				int parameterCount = method.getParameters().length;
+				Class<?>[] parameterTypes = method.getParameterTypes();
+				int parameterCount = parameterTypes.length;
 				int modifier = method.getModifiers();
 				if (parameterCount == 1) {
-					Parameter parameter = method.getParameters()[0];
-					if ("java.security.cert.Certificate".equals(parameter.getType().getName())
-							&& Modifier.isStatic(modifier) && method.getReturnType().equals(String.class)) {
-						samePinMethod = true;
+					Class parameter0Clz = parameterTypes[0];
+					if ("java.security.cert.Certificate".equals(parameter0Clz.getName()) && Modifier.isStatic(modifier)
+							&& method.getReturnType().equals(String.class)) {
+						hasPinMethod = true;
 					}
-					if ("java.security.cert.X509Certificate".equals(parameter.getType().getName())
-							&& Modifier.isStatic(modifier) 
-							&& !method.getReturnType().equals(Void.TYPE)) {
-						sameSha1Method = true;
+					if ("java.security.cert.X509Certificate".equals(parameter0Clz.getName())
+							&& Modifier.isStatic(modifier) && !method.getReturnType().equals(Void.TYPE)) {
+						hasSha1Method = true;
 					}
 				} else if (parameterCount == 2) {
-					Parameter parameter0 = method.getParameters()[0];
-					Parameter parameter1 = method.getParameters()[1];
-					if ("java.lang.String".equals(parameter0.getType().getName())
-							&& "java.util.List".equals(parameter1.getType().getName())
+					Class parameter0Clz = parameterTypes[0];
+					Class parameter1Clz = parameterTypes[1];
+					if ("java.lang.String".equals(parameter0Clz.getName())
+							&& "java.util.List".equals(parameter1Clz.getName())
 							&& method.getReturnType().equals(Void.TYPE)) {
-						sameCheckMethod = true;
-						checkMethodName = method.getName();
+						hasCheckMethod = true;
+						checkMethodName = method.toString();
 					}
 				}
 			}
-		} catch (Exception e) {
+		} catch (Throwable e) {
 		}
-		if (sameCheckMethod && samePinMethod && sameSha1Method) {
-			return new String[] { className, checkMethodName };
+		if (hasCheckMethod && hasPinMethod && hasSha1Method) {
+			Okhttp3FakeClass okhttp3FakeClass = new Okhttp3FakeClass("okhttp3.CertificatePinner", clz.getName());
+			okhttp3FakeClass.addFakeMethod(new FakeMethod(
+					"public void okhttp3.CertificatePinner.check(java.lang.String,java.util.List)", checkMethodName));
+			return okhttp3FakeClass;
 		}
 		return null;
 	}
 
-	private static boolean isInvalidOkHttpClass(String className) {
-		return className.startsWith("java.") || className.startsWith("android") || className.startsWith("com")
-				|| className.startsWith("[") || className.contains("$");
+	private static boolean isInvalidOkHttpClass(Class clz) {
+		String className = clz.getName();
+		return clz.isInterface() || clz.isArray() || clz.isAnnotation() || clz.isEnum() || className.startsWith("java.")
+				|| className.startsWith("android") || className.startsWith("com") || className.contains("$");
 	}
 
 	// public final class OkHostnameVerifier implements HostnameVerifier
-	public static String[] okHttpOkHostnameVerifierVerify(Class clz) {
-		if (isInvalidOkHttpClass(clz.getName())) {
-			return null;
-		}
+	public static Okhttp3FakeClass okHttpOkHostnameVerifierVerify(Class clz) {
 		// public boolean verify(String str, SSLSession sSLSession)
-		boolean sameVerify_Str_SSLSession_Method = false;
+		boolean hasVerify_Str_SSLSession_Method = false;
 		// public boolean verify(String str, X509Certificate x509Certificate)
-		boolean sameVerify_Str_X509Certificate_Method = false;
+		boolean hasVerify_Str_X509Certificate_Method = false;
 		// public static List<String> allSubjectAltNames(X509Certificate
 		// x509Certificate)
-		boolean same_AllSubjectAltNames_Method = false;
+		boolean has_AllSubjectAltNames_Method = false;
 
-		String sameVerify_Str_SSLSession_MethodName = null;
+		String hasVerify_Str_SSLSession_MethodName = null;
 
-		String sameVerify_Str_X509Certificate_MethodName = null;
+		String hasVerify_Str_X509Certificate_MethodName = null;
 		try {
+			if (isInvalidOkHttpClass(clz)) {
+				return null;
+			}
+			Class[] implInterfaces = clz.getInterfaces();
+			if (implInterfaces == null || implInterfaces.length != 1
+					|| !implInterfaces[0].equals(javax.net.ssl.HostnameVerifier.class)) {
+				return null;
+			}
 			if (!Modifier.isFinal(clz.getModifiers())) {
 				return null;
 			}
 			Method[] methods = clz.getDeclaredMethods();
 			for (int i = 0; i < methods.length; i++) {
 				Method method = methods[i];
-				int parameterCount = method.getParameterTypes().length;
+				Class<?>[] parameterTypes = method.getParameterTypes();
+				int parameterCount = parameterTypes.length;
 				int modifier = method.getModifiers();
 				if (parameterCount == 1) {
-					Parameter parameter = method.getParameters()[0];
-					if ("java.security.cert.X509Certificate".equals(parameter.getType().getName())
+					Class parameterClz = parameterTypes[0];
+					if ("java.security.cert.X509Certificate".equals(parameterClz.getName())
 							&& Modifier.isStatic(modifier)
 							&& method.getReturnType().getName().equals("java.util.List")) {
-						same_AllSubjectAltNames_Method = true;
+						has_AllSubjectAltNames_Method = true;
 					}
 				} else if (parameterCount == 2) {
-					Class parameter0Clz = method.getParameterTypes()[0];
-					Class parameter1Clz = method.getParameterTypes()[1];
+					Class parameter0Clz = parameterTypes[0];
+					Class parameter1Clz = parameterTypes[1];
 					if ("java.lang.String".equals(parameter0Clz.getName())
 							&& "javax.net.ssl.SSLSession".equals(parameter1Clz.getName())
 							&& method.getReturnType().equals(boolean.class)) {
-						sameVerify_Str_SSLSession_Method = true;
-						sameVerify_Str_SSLSession_MethodName = method.getName();
+						hasVerify_Str_SSLSession_Method = true;
+						hasVerify_Str_SSLSession_MethodName = method.toString();
 					}
 					if ("java.lang.String".equals(parameter0Clz.getName())
 							&& "java.security.cert.X509Certificate".equals(parameter1Clz.getName())
 							&& method.getReturnType().equals(boolean.class)) {
-						sameVerify_Str_X509Certificate_Method = true;
-						sameVerify_Str_X509Certificate_MethodName = method.getName();
+						hasVerify_Str_X509Certificate_Method = true;
+						hasVerify_Str_X509Certificate_MethodName = method.toString();
 					}
 				}
 			}
-		} catch (Exception e) {
+		} catch (Throwable e) {
 		}
-		if (sameVerify_Str_SSLSession_Method && sameVerify_Str_X509Certificate_Method
-				&& same_AllSubjectAltNames_Method) {
-			return new String[] { clz.getName(), sameVerify_Str_SSLSession_MethodName,
-					sameVerify_Str_X509Certificate_MethodName };
+		if (hasVerify_Str_SSLSession_Method && hasVerify_Str_X509Certificate_Method && has_AllSubjectAltNames_Method) {
+			Okhttp3FakeClass okhttp3FakeClass = new Okhttp3FakeClass("okhttp3.internal.tls.OkHostnameVerifier",
+					clz.getName());
+			okhttp3FakeClass.addFakeMethod(new FakeMethod(
+					"public boolean okhttp3.internal.tls.OkHostnameVerifier.verify(java.lang.String,javax.net.ssl.SSLSession)",
+					hasVerify_Str_SSLSession_MethodName));
+			okhttp3FakeClass.addFakeMethod(new FakeMethod(
+					"public boolean okhttp3.internal.tls.OkHostnameVerifier.verify(java.lang.String,java.security.cert.X509Certificate)",
+					hasVerify_Str_X509Certificate_MethodName));
+			return okhttp3FakeClass;
 		}
 		return null;
 	}
